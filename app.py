342319bc32f6e50d89e279a0b16ffe1e603cf63a
Salvino Modica <salvymody@gmail.com>
2024-06-09 19:45:19 +0000

    feat: add page_5

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import streamlit as st
 from app_pages.multi_page import MultiPage
+
 from app_pages.page_1_summary import page_1_summary_body
 from app_pages.page_2_sale_price_study import page_2_sale_price_study_body
 from app_pages.page_3_sale_price_prediction import page_3_sale_price_prediction_body
 from app_pages.page_4_hypothesis_and_validation import page_4_hypothesis_validation_body
+from app_pages.page_5_ml_model import page_5_ml_model_body
 
 app = MultiPage(app_name= "Temporary Name")
 
@@ -25,5 +27,6 @@ app.app_page("Quick Project Summary", page_1_summary_body)
 app.app_page("Sale Price Study", page_2_sale_price_study_body)
 app.app_page("Sale Price Prediction", page_3_sale_price_prediction_body)
 app.app_page("Hypothesis and Validation", page_4_hypothesis_validation_body)
+app.app_page("Machine Learning Model", page_5_ml_model_body)
 
 app.run()
---
+++ app_pages/page_5_ml_model.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def page_5_ml_model_body():
+
+    st.write("# Machine Learning Model")
