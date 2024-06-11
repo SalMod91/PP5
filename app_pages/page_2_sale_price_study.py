b99b6d7995159e3e0adb1d0b664cbeca746c7a1e
Salvino Modica <salvymody@gmail.com>
2024-06-11 12:44:05 +0000

    feat: add inspect dataset to page_2

--- app_pages/page_2_sale_price_study.py
+++ app_pages/page_2_sale_price_study.py
@@ -22,3 +22,77 @@ def page_2_sale_price_study_body():
             f"Displayed below are the first 10 rows of the dataset.")
         
         st.write(df.head(10))
+
+        st.write(
+        """
+        ### Data Dictionary:
+
+        Below is a detailed explanation of each variable in our dataset.
+
+        - **1stFlrSF**: First Floor square feet
+        - **2ndFlrSF**: Second floor square feet
+        - **BedroomAbvGr**: Bedrooms above grade (does NOT include basement bedrooms)
+        - **BsmtExposure**: Walkout or garden level walls
+            - *Gd*: Good Exposure
+            - *Av*: Average Exposure
+            - *Mn*: Minimum Exposure
+            - *No*: No Exposure
+            - *None*: No Basement
+        - **BsmtFinSF1**: Type 1 finished square feet
+        - **BsmtFinType1**: Rating of basement finished area
+            - *GLQ*: Good Living Quarters
+            - *ALQ*: Average Living Quarters
+            - *BLQ*: Below Average Living Quarters
+            - *Rec*: Average Rec Room
+            - *LwQ*: Low Quality
+            - *Unf*: Unfinished
+            - *None*: No Basement
+        - **BsmtUnfSF**: Unfinished square feet of basement area
+        - **EnclosedPorch**: Enclosed porch area in square feet
+        - **GarageArea**: Size of garage in square feet
+        - **GarageFinish**: Interior finish of the garage
+            - *Fin*: Finished
+            - *RFn*: Rough Finished
+            - *Unf*: Unfinished
+            - *None*: No Garage
+        - **GarageYrBlt**: Year garage was built
+        - **GrLivArea**: Above grade (ground) living area square feet
+        - **TotalBsmtSF**: Total square feet of basement area
+        - **KitchenQual**: Kitchen quality
+            - *Ex*: Excellent
+            - *Gd*: Good
+            - *TA*: Typical/Average
+            - *Fa*: Fair
+            - *Po*: Poor
+        - **LotArea**: Lot size in square feet
+        - **LotFrontage**: Linear feet of street connected to property
+        - **MasVnrArea**: Masonry veneer area in square feet
+        - **OpenPorchSF**: Open porch area in square feet
+        - **OverallCond**: Rates the overall condition of the house
+            - *10*: Very Excellent
+            - *9*: Excellent
+            - *8*: Very Good
+            - *7*: Good
+            - *6*: Above Average
+            - *5*: Average
+            - *4*: Below Average
+            - *3*: Fair
+            - *2*: Poor
+            - *1*: Very Poor
+        - **OverallQual**: Rates the overall material and finish of the house
+            - *10*: Very Excellent
+            - *9*: Excellent
+            - *8*: Very Good
+            - *7*: Good
+            - *6*: Above Average
+            - *5*: Average
+            - *4*: Below Average
+            - *3*: Fair
+            - *2*: Poor
+            - *1*: Very Poor
+        - **WoodDeckSF**: Wood deck area in square feet
+        - **YearBuilt**: Original construction date
+        - **YearRemodAdd**: Remodel date (same as construction date if no remodelling or additions)
+        - **SalePrice**: Sale Price
+        """
+    )
