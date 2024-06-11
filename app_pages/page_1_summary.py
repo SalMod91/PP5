be4eff8f6b93e21a7f52f5e7faa4711d08f88c6f
Salvino Modica <salvymody@gmail.com>
2024-06-11 12:49:06 +0000

    refactor: changed page_1 from f-string to multi-line format

--- app_pages/page_1_summary.py
+++ app_pages/page_1_summary.py
@@ -10,37 +10,33 @@ def page_1_summary_body():
     st.write("## Quick Project Summary")
 
     # text based on README file - "Dataset Content" section
-    st.info(
-        f"### **Project Terms**\n\n"
-        f"A client has inherited four properties and seeks to accurately assess and "
-        f"maximize their sales prices. The focus is on ensuring that these properties "
-        f"are evaluated accurately and priced competitively according to the local real "
-        f"estate market conditions in Ames, Iowa, USA.\n\n"
-        f"### **Project Jargons**\n\n"
-        f"* **Target Variable/Target**: This is the main result or outcome that our model tries to predict.\n"
-        f"* **Features/Attributes/Variables**: These words describe the characteristics "
-        f"of a property and are used interchangeably to refer to the input data that "
-        f"influence the target variable in our predictive model.\n"
-        f"* **SalePrice**:  This is the price at which a house was sold. It is our "
-        f"target variable.\n"
-        f"* **Property/House**: These terms are used interchangeably to refer to the "
-        f"sales records documented in the dataset.\n\n"    
-        f"### **Project Dataset**\n\n"
-        f"The dataset contains housing records from Ames, Iowa, detailing various "
-        f"characteristics of each house. These characteristics are known as feature variables "
-        f"and they include:\n"
-        f"* **Floor Area**: Total floor space of the house\n\n"
-        f"* **Basement**: Details about the basement area\n\n"
-        f"* **Garage**: Garage size and capacity\n\n"
-        f"* **Lot**: Size of the lot on which the house is built\n\n"
-        f"* **Porch**: Size and type of porch\n\n"
-        f"* **Wood Deck**: Details about the wood deck, if any\n\n"
-        f"* **Year Built**: The year in which the house was constructed\n\n"
-        f"These features help us understand and model how different aspects of a house may "
-        f"influence its sale price. The dataset spans houses built between 1872 and 2010.\n\n"
-        f"For a more detailed understanding of the dataset and definitions of the terms used, "
-        f"please click **[HERE](https://www.kaggle.com/datasets/codeinstitute/housing-prices-data)**.\n\n"
-        )
+    st.info("""
+    ### **Project Terms**
+
+    A client has inherited four properties and seeks to accurately assess and maximize their sales prices. The focus is on ensuring that these properties are evaluated accurately and priced competitively according to the local real estate market conditions in Ames, Iowa, USA.
+
+    ### **Project Jargons**
+
+    * **Target Variable/Target**: This is the main result or outcome that our model tries to predict.
+    * **Features/Attributes/Variables**: These words describe the characteristics of a property and are used interchangeably to refer to the input data that influence the target variable in our predictive model.
+    * **SalePrice**:  This is the price at which a house was sold. It is our target variable.
+    * **Property/House**: These terms are used interchangeably to refer to the sales records documented in the dataset.
+
+    ### **Project Dataset**
+
+    The dataset contains housing records from Ames, Iowa, detailing various characteristics of each house. These characteristics are known as feature variables and they include:
+    * **Floor Area**: Total floor space of the house
+    * **Basement**: Details about the basement area
+    * **Garage**: Garage size and capacity
+    * **Lot**: Size of the lot on which the house is built
+    * **Porch**: Size and type of porch
+    * **Wood Deck**: Details about the wood deck, if any
+    * **Year Built**: The year in which the house was constructed
+
+    These features help us understand and model how different aspects of a house may influence its sale price. The dataset spans houses built between 1872 and 2010.
+
+    For a more detailed understanding of the dataset and definitions of the terms used, please click **[HERE](https://www.kaggle.com/datasets/codeinstitute/housing-prices-data)**.
+    """)
 
     # Provide a link to the full project documentation
     st.write(
@@ -49,18 +45,13 @@ def page_1_summary_body():
         )
 
     # copied from README file - "Business Requirements" section
-    st.success(
-        f"### **Business Requirements**:\n\n"
-        f"1. **Correlation Analysis**:  The client seeks to understand how various house "
-        f"attributes correlate with the Sale Price. They expect the dashboard to feature "
-        f"data visualizations that illustrate the relationships between these attributes "
-        f"and the Sale Price.\n\n"
-        f"2. **Price Prediction**: The client aims to predict the sale prices of four "
-        f"inherited properties, as well as other houses in Ames, Iowa, using the "
-        f"developed model.\n\n"
-        f"**Performance Goals**:\n"
-        f"To ensure the model meets the client's needs, we have established a performance "
-        f"goal with the client that the model should achieve an R² score of at least 0.75. "
-        f"This score must be maintained both on the training dataset and on the test "
-        f"dataset."
-        )
+    st.success("""
+    ### **Business Requirements**:
+
+    1. **Correlation Analysis**: The client seeks to understand how various house attributes correlate with the Sale Price. They expect the dashboard to feature data visualizations that illustrate the relationships between these attributes and the Sale Price.
+
+    2. **Price Prediction**: The client aims to predict the sale prices of four inherited properties, as well as other houses in Ames, Iowa, using the developed model.
+
+    **Performance Goals**:
+    To ensure the model meets the client's needs, we have established a performance goal with the client that the model should achieve an R² score of at least 0.75. This score must be maintained both on the training dataset and on the test dataset.
+    """)
