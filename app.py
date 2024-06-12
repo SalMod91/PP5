13b0a36c6dd416fed3980b910774e2718db40b03
Salvino Modica <salvymody@gmail.com>
2024-06-12 12:25:22 +0000

    refactor: changed app name

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from app_pages.page_3_sale_price_prediction import page_3_sale_price_prediction_
 from app_pages.page_4_hypothesis_and_validation import page_4_hypothesis_validation_body
 from app_pages.page_5_ml_model import page_5_ml_model_body
 
-app = MultiPage(app_name= "Temporary Name")
+app = MultiPage(app_name= "Housing Sale Price Predictor")
 
 app.app_page("Quick Project Summary", page_1_summary_body)
 app.app_page("Sale Price Study", page_2_sale_price_study_body)
